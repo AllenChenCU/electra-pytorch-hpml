3449559c884723e09fbb726aa1289301b6ba5095
Allen Chen <atc2160@columbia.edu>
2024-05-01 17:29:28 -0700

    run original

--- examples/glue/run.sh
+++ examples/glue/run.sh
@@ -1,36 +1,36 @@
 
 
 # un-finetuned
-python examples/glue/run.py \
-  --model_name_or_path google/electra-small-discriminator \
-  --task_name MRPC \
-  --do_train False \
-  --do_eval True \
-  --data_dir data/glue_data/MRPC \
-  --max_seq_length 128 \
-  --per_gpu_train_batch_size 32 \
-  --learning_rate 2e-5 \
-  --num_train_epochs 2.0 \
-  --output_dir output/unfinetuned \
-  --overwrite_output_dir True \
-  --cache_dir electra_small_cache \
-  --finetune_method original
-
-# Command for the original run
 # python examples/glue/run.py \
 #   --model_name_or_path google/electra-small-discriminator \
 #   --task_name MRPC \
-#   --do_train True \
+#   --do_train False \
 #   --do_eval True \
 #   --data_dir data/glue_data/MRPC \
 #   --max_seq_length 128 \
 #   --per_gpu_train_batch_size 32 \
 #   --learning_rate 2e-5 \
 #   --num_train_epochs 2.0 \
-#   --output_dir output/original \
+#   --output_dir output/unfinetuned \
 #   --overwrite_output_dir True \
 #   --cache_dir electra_small_cache \
 #   --finetune_method original
+
+# Command for the original run
+python examples/glue/run.py \
+  --model_name_or_path google/electra-small-discriminator \
+  --task_name MRPC \
+  --do_train True \
+  --do_eval True \
+  --data_dir data/glue_data/MRPC \
+  --max_seq_length 128 \
+  --per_gpu_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 2.0 \
+  --output_dir output/original \
+  --overwrite_output_dir True \
+  --cache_dir electra_small_cache \
+  --finetune_method original
 
 # Command for the LoRA run
 # python examples/glue/run.py \
