ab8b0e77b7c6fb4d2d6914759d7954ca23232db8
Allen Chen <atc2160@columbia.edu>
2024-05-01 23:56:14 -0700

    fix config

--- examples/glue/run.sh
+++ examples/glue/run.sh
@@ -32,20 +32,20 @@ python examples/glue/run.py \
 #   --finetune_method original
 
 # # Command for the original run
-# python examples/glue/run.py \
-#   --model_name_or_path google/electra-small-discriminator \
-#   --task_name MRPC \
-#   --do_train True \
-#   --do_eval True \
-#   --data_dir data/glue_data/MRPC \
-#   --max_seq_length 128 \
-#   --per_gpu_train_batch_size 32 \
-#   --learning_rate 2e-5 \
-#   --num_train_epochs 15.0 \
-#   --output_dir output/original \
-#   --overwrite_output_dir True \
-#   --cache_dir electra_small_cache \
-#   --finetune_method original
+python examples/glue/run.py \
+  --model_name_or_path bert-base-uncased \
+  --task_name MRPC \
+  --do_train True \
+  --do_eval True \
+  --data_dir data/glue_data/MRPC \
+  --max_seq_length 128 \
+  --per_gpu_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 15.0 \
+  --output_dir output/original \
+  --overwrite_output_dir True \
+  --cache_dir electra_small_cache \
+  --finetune_method original
 
 # Command for the LoRA run
 python examples/glue/run.py \
