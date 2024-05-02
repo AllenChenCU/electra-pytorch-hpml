5673a0e7099ffa5722f3d48ecc0deed4b3dccdad
Allen Chen <atc2160@columbia.edu>
2024-05-02 13:24:21 -0700

    fix bug

--- examples/glue/run.sh
+++ examples/glue/run.sh
@@ -1,19 +1,19 @@
 
 # warmup
-python examples/glue/run.py \
-  --model_name_or_path google/electra-small-discriminator \
-  --task_name MRPC \
-  --do_train False \
-  --do_eval True \
-  --data_dir data/glue_data/MRPC \
-  --max_seq_length 128 \
-  --per_gpu_train_batch_size 32 \
-  --learning_rate 2e-5 \
-  --num_train_epochs 20.0 \
-  --output_dir output/warmup \
-  --overwrite_output_dir True \
-  --cache_dir electra_small_cache \
-  --finetune_method original
+# python examples/glue/run.py \
+#   --model_name_or_path google/electra-small-discriminator \
+#   --task_name MRPC \
+#   --do_train False \
+#   --do_eval True \
+#   --data_dir data/glue_data/MRPC \
+#   --max_seq_length 128 \
+#   --per_gpu_train_batch_size 32 \
+#   --learning_rate 2e-5 \
+#   --num_train_epochs 20.0 \
+#   --output_dir output/warmup \
+#   --overwrite_output_dir True \
+#   --cache_dir electra_small_cache \
+#   --finetune_method original
 
 # # un-finetuned
 # python examples/glue/run.py \
