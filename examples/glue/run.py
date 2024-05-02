306a6c9c9fc56a1a4e6608d6dc73308b9179c4f2
Allen Chen <atc2160@columbia.edu>
2024-05-02 12:46:18 -0700

    add storage

--- examples/glue/run.py
+++ examples/glue/run.py
@@ -240,6 +240,7 @@ def main(task='MRPC', seed=42, ckpt='google/electra-small-discriminator'):
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type="nf4", 
             bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_quant_storage=torch.bfloat16,
         )
     else:
         bnb_config = None
