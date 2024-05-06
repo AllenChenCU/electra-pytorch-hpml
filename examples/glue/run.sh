55ad21cfd80e765cb3d09a4514c9721ea309784d
Allen Chen <atc2160@columbia.edu>
2024-05-06 16:51:00 -0700

    fix bug

--- examples/glue/run.sh
+++ examples/glue/run.sh
@@ -307,7 +307,7 @@ python3 examples/glue/run.py \
 #   --prune_dim 1
 
 # Tuning the prune amount
-max_prune_amt = 65
+max_prune_amt=65
 for ((i=25; i<=max_prune_amt; i+=10)); do
   output_dir="output/unstructured_l_l1_$i"
   # Unstructured layer l1 @ prune amt i
